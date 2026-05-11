79d9e287395068f1ffdab53009ce6003346b1e21
señorMesa <sergioperez.iecc@gmail.com>
2026-05-11 17:46:02 +0100

    chore: create basic structure folder for the project

--- src/main/java/com/pen/taskmanagement/model/Task.java
+++ src/main/java/com/pen/taskmanagement/model/Task.java
@@ -3,14 +3,25 @@ package com.pen.taskmanagement.model;
 import java.time.LocalDateTime;
 
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 
 @Entity
-
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Task {
     
     private Long id;
     private String name;
-    private LocalDateTime timestamp;
+    private LocalDateTime starTime;
+    private LocalDateTime endTime;
+
+
+    
 
 }
