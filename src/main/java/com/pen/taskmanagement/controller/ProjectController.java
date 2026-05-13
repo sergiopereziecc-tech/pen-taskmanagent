0b070c5828cc6047852417b0775ef5bae5802a20
señorMesa <sergioperez.iecc@gmail.com>
2026-05-13 17:43:09 +0100

    feat: add project controller with endpoints and httpstatus reply

---
+++ src/main/java/com/pen/taskmanagement/controller/ProjectController.java
@@ -0,0 +1,55 @@
+package com.pen.taskmanagement.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pen.taskmanagement.dtos.ProjectRequest;
+import com.pen.taskmanagement.dtos.ProjectResponse;
+import com.pen.taskmanagement.service.ProjectService;
+
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+@RestController
+@RequestMapping("/api/project")
+@RequiredArgsConstructor
+public class ProjectController {
+    
+    private final ProjectService projectService;
+
+
+    @PostMapping("/new")
+    public ResponseEntity<ProjectResponse> createProject(@RequestBody ProjectRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(projectService.createProject(request));        
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<ProjectResponse> getProject(@PathVariable Long id) {
+        return ResponseEntity.ok(projectService.readProject(id));
+    }
+    @GetMapping("/all")
+    public ResponseEntity<List<ProjectResponse>> getProjectList() {
+        return ResponseEntity.ok(projectService.readProjects());
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProject(@PathVariable Long id){
+        projectService.deleteProject(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+    @PatchMapping("/{id}")
+    public ResponseEntity<ProjectResponse> updateProject(@PathVariable Long id, @RequestBody ProjectRequest request){
+        return ResponseEntity.ok(projectService.updateProject(request, id));
+    }
+}
