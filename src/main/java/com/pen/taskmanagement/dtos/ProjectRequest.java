b09315078252115ce678f607816aa5930c77e03c
señorMesa <sergioperez.iecc@gmail.com>
2026-05-14 10:21:21 +0100

    chore: final validation touches

--- src/main/java/com/pen/taskmanagement/dtos/ProjectRequest.java
+++ src/main/java/com/pen/taskmanagement/dtos/ProjectRequest.java
@@ -6,8 +6,12 @@ import java.util.List;
 import com.pen.taskmanagement.model.ProjectStatus;
 import com.pen.taskmanagement.model.User;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record ProjectRequest(
+    @NotBlank
     String name,
+    @NotBlank
     String description,
     LocalDateTime endDateTime,
     List<Long> userIds
