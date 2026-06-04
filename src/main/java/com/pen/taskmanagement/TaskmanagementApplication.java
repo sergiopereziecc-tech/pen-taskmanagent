e6f9eb830717cb40236eb815007352c4bebeb0ed
señorMesa <sergioperez.iecc@gmail.com>
2026-06-04 20:29:51 +0100

    refactor: add method to main to load .env variables to avoid hardcoded data

--- src/main/java/com/pen/taskmanagement/TaskmanagementApplication.java
+++ src/main/java/com/pen/taskmanagement/TaskmanagementApplication.java
@@ -3,10 +3,18 @@ package com.pen.taskmanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvEntry;
+
 @SpringBootApplication
 public class TaskmanagementApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().load();
+		for (DotenvEntry doten : dotenv.entries()) {
+			System.setProperty(doten.getKey(), doten.getValue());
+		}
 		SpringApplication.run(TaskmanagementApplication.class, args);
 	}
 
