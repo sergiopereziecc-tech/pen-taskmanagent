77cdf31bc3e0466eaa3b15e8d206166306b43cf2
señorMesa <sergioperez.iecc@gmail.com>
2026-06-07 10:49:49 +0100

    refactor: clean path urls to match Rest convention

--- src/main/java/com/pen/taskmanagement/controller/TaskController.java
+++ src/main/java/com/pen/taskmanagement/controller/TaskController.java
@@ -16,14 +16,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
-
-
 
 @RestController
 @RequestMapping("/api/task")
@@ -32,35 +31,32 @@ public class TaskController {
 
     private final TaskService taskService;
 
-
-    @PostMapping("/new")
+    @PostMapping
     public ResponseEntity<TaskResponse> createTask(@Valid @RequestBody TaskRequest request) {
-        
-        
+
         return ResponseEntity.status(HttpStatus.CREATED).body(taskService.createTask(request));
     }
 
-    @GetMapping("/all/tasks")
+    @GetMapping
     public ResponseEntity<List<TaskResponse>> getListTasks() {
         return ResponseEntity.ok(taskService.readAllTask());
     }
 
-    @GetMapping("/task/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<TaskResponse> getTask(@PathVariable Long id) {
         return ResponseEntity.ok(taskService.readTask(id));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteTask(@PathVariable Long id){
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
         taskService.deleteTask(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
-    @PatchMapping("/update/{id}")
-    public ResponseEntity<TaskResponse> updateTask(@PathVariable Long id, @Valid @RequestBody TaskRequest request){
+    @PutMapping("/{id}")
+    public ResponseEntity<TaskResponse> updateTask(@PathVariable Long id, @Valid @RequestBody TaskRequest request) {
 
         return ResponseEntity.status(HttpStatus.OK).body(taskService.updateTask(request, id));
     }
-    
-    
+
 }
