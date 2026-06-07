19e53e9cb6d0a0a11b13b7710a0cb87ce709587a
señorMesa <sergioperez.iecc@gmail.com>
2026-06-07 09:35:37 +0100

    refactor: add colum annotation on username to protect unique usernames

--- src/main/java/com/pen/taskmanagement/model/User.java
+++ src/main/java/com/pen/taskmanagement/model/User.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.Mapping;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -39,6 +40,7 @@ public class User implements UserDetails{
     @NotBlank
     private String email;
     @NotBlank
+    @Column(unique = true)
     private String username;
     private String password;
 
