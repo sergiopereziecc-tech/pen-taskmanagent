d6b7c682601696b89aefd5b4fb02ece0f38b472b
señorMesa <sergioperez.iecc@gmail.com>
2026-06-08 11:56:08 +0100

    feat: add Role authorisation to SecurityConfig protecting endpoints

--- src/main/java/com/pen/taskmanagement/config/SecurityConfig.java
+++ src/main/java/com/pen/taskmanagement/config/SecurityConfig.java
@@ -2,12 +2,14 @@ package com.pen.taskmanagement.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.pen.taskmanagement.model.RoleEnum;
 import com.pen.taskmanagement.utilities.JwtFilter;
 
 import lombok.RequiredArgsConstructor;
@@ -29,6 +31,12 @@ public class SecurityConfig {
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/auth/register").permitAll()
                 .requestMatchers("/api/auth/login").permitAll()
+                .requestMatchers(HttpMethod.POST,"/api/project").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PUT,"/api/project/{id}").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE,"/api/project/{id}").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.POST,"/api/task").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PUT,"/api/task/{id}").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE,"/api/task/{id}").hasRole("ADMIN")
                 .anyRequest().authenticated())
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
