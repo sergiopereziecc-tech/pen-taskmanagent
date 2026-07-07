6f0297d92e79d6c7763df7d433dfc1f78c91be86
señorMesa <sergioperez.iecc@gmail.com>
2026-07-07 09:11:43 +0100

    fix: remove unused imports from TaskManagemntApplication

--- src/main/java/com/pen/taskmanagement/TaskmanagementApplication.java
+++ src/main/java/com/pen/taskmanagement/TaskmanagementApplication.java
@@ -3,8 +3,7 @@ package com.pen.taskmanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
-import io.github.cdimascio.dotenv.DotenvEntry;
+
 
 @SpringBootApplication
 public class TaskmanagementApplication {
